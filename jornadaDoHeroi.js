cc16bac781bc78e98a40e29a745fb6763b5b75e3
Hiagodsl <hiagodaslacerda@gmail.com>
2024-06-12 19:41:02 -0300

    Primeiro comit

---
+++ jornadaDoHeroi.js
@@ -0,0 +1,27 @@
+let nomeHeroi ="A"
+let xpHeroi = 2300
+
+informacoesHeroi = [nomeHeroi,xpHeroi]
+
+if (xpHeroi <= 1000){
+    console.log("O Heroi de nome " + informacoesHeroi[0] + " está no nivel de Ferro")
+} else if (informacoesHeroi[1] => 10001 || informacoesHeroi[1] <= 2000){
+    console.log("O Heroi de nome " + informacoesHeroi[0] + " está no nivel de Bronze" )
+}else if (xpHeroi => 2001 || xpHeroi <= 5000){
+    console.log("O Heroi de nome " + informacoesHeroi[0] +" está no nivel de Prata")
+} else if(xpHeroi => 5001 || xpHeroi <= 7000){
+    console.log("O Heroi de nome " + informacoesHeroi[0] + " está no nivel de Ouro")
+}else if (xpHeroi => 7001 || xpHeroi <= 8000){
+    console.log("O Heroi de nome " + informacoesHeroi[0] + " está no nivel de Platina")
+}else if (xpHeroi >= 8001 || xpHeroi <= 9000 ){
+    console.log("O Heroi de nome " + informacoesHeroi[0] + " está no nivel de Ascendente")
+}else if (xpHeroi => 9001 || xpHeroi <= 10000 ){
+    console.log("O Heroi de nome " + informacoesHeroi[0] + " está no nivel de Imortal")
+} else if (xpHeroi => 10001){
+    console.log("O Heroi de nome " + informacoesHeroi[0] + " está no nivel de Radiante")
+}
+
+
+
+
+informacoesHeroi[1]
